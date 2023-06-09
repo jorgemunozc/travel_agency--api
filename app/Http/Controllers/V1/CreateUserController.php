e9fe24d5e1f258bc097740fde4afbb643961d0fb
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2023-06-09 11:27:31 -0400

    refactor: move resource to v1 api

--- app/Http/Controllers/V1/CreateUserController.php
+++ app/Http/Controllers/V1/CreateUserController.php
@@ -6,7 +6,7 @@ namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\CreateUserRequest;
-use App\Http\Resources\UserResource;
+use App\Http\Resources\V1\UserResource;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 
