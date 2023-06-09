8b4eb0b8cf1c9c73110572bc7a580fc993c1fdd3
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2023-06-09 02:00:45 -0400

    feat: added routes for listing tours and travels

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\CreateUserController;
+use App\Http\Controllers\TourController;
 use App\Http\Controllers\TravelController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/travels', [TravelController::class, 'index']);
+Route::get('/travels/{travel:slug}/tours', [TourController::class, 'index']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/users', CreateUserController::class);
     Route::post('/travels', [TravelController::class, 'store']);
