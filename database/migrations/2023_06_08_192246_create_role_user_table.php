83416f086af237218b01606a72d428046ee341d2
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2023-06-08 20:35:01 -0400

    refactor: fix table name

--- database/migrations/2023_06_08_192246_create_role_user_table.php
+++ database/migrations/2023_06_08_192246_create_role_user_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('rol_user', function (Blueprint $table) {
+        Schema::create('role_user', function (Blueprint $table) {
             $table->foreignUuid('user_id')->constrained();
             $table->foreignUuid('role_id')->constrained();
             $table->timestamps();
