f91d0ac3bb3cb9bbaf7d3c9f9c0fd2786902930d
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2023-06-08 20:38:14 -0400

    refactor: added route to create travels

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\CreateUserController;
+use App\Http\Controllers\TravelController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,7 +18,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->group(function () {
-    // Route::resource('/users');
-    // Route::resource(('/travels'));
-    // Route::resource('travels.tour');
+    Route::post('/users', CreateUserController::class);
+    Route::post('/travels', [TravelController::class, 'store']);
 });
