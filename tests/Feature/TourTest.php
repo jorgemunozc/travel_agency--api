f83ce6dc24452682f1d818fec5fd57651a849628
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2023-06-09 02:02:16 -0400

    feat: added test for showing public lists of tours

---
+++ tests/Feature/TourTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use App\Models\Tour;
+use App\Models\Travel;
+use Tests\TestCase;
+
+class TourTest extends TestCase
+{
+    public function testVisitorCanSeeToursForSpecifiedTravel()
+    {
+        $travel = Travel::factory()
+            ->public()
+            ->has(Tour::factory()->count(3))
+            ->create();
+        $response = $this->getJson("/api/travels/{$travel->slug}/tours");
+        $response->assertAccepted()
+            ->assertJsonCount($travel->loadCount('tours')->tours_count, 'data');
+    }
+
+    public function testVisitorCanOnlySeeToursForPublicTravel()
+    {
+        $travel = Travel::factory()
+            ->private()
+            ->has(Tour::factory()->count(fake()->randomDigit()))
+            ->create();
+        $response = $this->getJson("api/travels/{$travel->slug}/tours");
+        $response->assertForbidden();
+    }
+
+    public function testAdminCanSeeToursForAnyTravel()
+    {
+        $travels = Travel::factory()
+            ->has(Tour::factory()->count(fake()->randomDigit()))
+            ->count(fake()->randomDigit())
+            ->create();
+        $travel = $travels->random();
+        $response = $this
+            ->actingAs($this->loginAs())
+            ->getJson("/api/travels/{$travel->slug}/tours");
+        $response->assertAccepted();
+    }
+}
